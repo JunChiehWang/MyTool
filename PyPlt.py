ff51c73bb790f7ee87362b3d0b44515fc3cb5fea
Jun-Chieh Wang <JunChieh_Wang@amat.com>
2019-11-27 23:40:47 -0800

    minor tweak

--- PyPlt.py
+++ PyPlt.py
@@ -4,17 +4,15 @@ import seaborn as sns
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-
+from platform import python_version
 
 # print version of packages
 print("Import PyPlt:")
-from platform import python_version
 print("python version: ", python_version())
 print("numpy version: ", np.version.version)
 print("pandas version: ", pd.__version__)
 print("matplotlib version: ", matplotlib.__version__)
 print("Seaborn version: ", sns.__version__)
-
 
 class MyPlt:
     """My python plotter library.
@@ -26,7 +24,7 @@ class MyPlt:
         data_x: data for x axis
         data_y: data for y on left y axis
         data_y2: data for y2 on right y axis (optional), default = None
-        xlabel, ylabel, y2label = label for x, y ,y2,
+        xlabel, ylabel, y2label = label for data_x, data_y , data_y2,
             default = None if numpy array or list, or
                       name of data if input data is a pandas series
         xmin, xmax, ymin, ymax, y2min, y2max: min and max of x, y, y2 axis,
@@ -36,7 +34,7 @@ class MyPlt:
             name = title + "png", default = False
         **fig_kw: keywords that are passed to matplotlib.pyplot.plot
     # Date
-        20191125
+        20191127
     """
 
     def __init__(self, data_x, data_y, data_y2=None,
@@ -44,8 +42,6 @@ class MyPlt:
                  ymin=None, ymax=None, y2min=None, y2max=None,
                  xlabel=None, ylabel=None, y2label=None,
                  title=None, savefig=False):
-
-
 
         # check if 2 y axis
         self.double_y = False
@@ -112,6 +108,7 @@ class MyPlt:
         self.tkw = dict(size=4, width=1.5)
 
     def scatter_plt(self, hue=None, size=None, style=None,
+                    set_ylabel=None,
                     markersize=50, edgecolor='k', legend='brief',
                     **fig_kw):
         """Scatter plot.
@@ -122,35 +119,64 @@ class MyPlt:
             markersize: marker size, default = 50. it has no effect if size is
                 used.
             edgecolor : color of marks, default = 'k' (black)
+            set_ylabel: used for set_ylabel to set label for y axis
+                None: don't set_ylabel, default
+                'self': use set_ylabel(self.ylabel)
+                other string: use set_ylabel(set_ylabel)
             legend : “brief”, “full”, or False, default = 'brief'
             **fig_kw: keywords that are passed to matplotlib.pyplot.plot
         # Example
+            (1)
             fig, ax = plt.subplots(2,1,figsize=(14,16))
             a = PyPlt.MyPlt(x, y, xmin=0.1, xmax=5, ymin=2, ymax=8)
             a.scatter_plt(style=y,marksize=100,edgecolor='red',legend='brief',ax=ax[0])
             a.scatter_plt(hue=x,ax=ax[1])
+
+            (2) 2 scatter plots on 1 figure
+            fig, ax = plt.subplots(5,1,figsize=(8,16))
+            a = PyPlt.MyPlt(x, y, xmin=0.1, xmax=5, ymin=2, ymax=18,
+                            xlabel='x', ylabel='y ylabel', title='a title')
+            a.scatter_plt(markersize=100, edgecolor='red', legend='brief',
+                          ax=ax[0])
+            a = PyPlt.MyPlt(x, y2, xmin=0.1, xmax=5, ymin=2, ymax=18,
+                            xlabel='x', ylabel='y2 ylabel', title='a title')
+            a.scatter_plt(markersize=100, edgecolor='blue', legend='brief',
+                          ax=ax[0])
         # Date
-            20191125
+            20191127
     """
+        plot_ylabel = self.ylabel
+
         if hue is not None:
             assert len(hue) == self.data_size
+            plot_ylabel = None
         if size is not None:
             assert len(size) == self.data_size
+            plot_ylabel = None
         if style is not None:
             assert len(style) == self.data_size
+            plot_ylabel = None
 
+        # lebel here will be shown in legend
         plot = sns.scatterplot(self.x, self.y,
                                hue=hue, size=size, style=style,
                                s=markersize, edgecolor=edgecolor,
-                               legend=legend, **fig_kw)
+                               legend=legend, label=plot_ylabel,
+                               **fig_kw)
 
         # set x, y limit
         plot.set_xlim(self.xmin, self.xmax)
         plot.set_ylim(self.ymin, self.ymax)
 
-        # set labels
+        # set labels, shown on axis
         plot.set_xlabel(self.xlabel)
-        plot.set_ylabel(self.ylabel)
+
+        if set_ylabel is None:
+            plot.set_ylabel('')
+        elif set_ylabel == 'self':
+            plot.set_ylabel(self.ylabel)
+        else:
+            plot.set_ylabel(set_ylabel)
 
         # set ticks
         plot.tick_params(axis='x', **self.tkw)
@@ -203,7 +229,7 @@ class MyPlt:
             Eth_rint.box_plt(ax=ax[1], hue=df_Eth['case'], boxvalue=True,
                              linewidth=1, width=0.7)
         # Date
-            20191126
+            20191127
     """
         if hue is not None:
             assert len(hue) == self.data_size
@@ -213,7 +239,7 @@ class MyPlt:
                            linewidth=linewidth, width=width,
                            **fig_kw)
 
-        ## set x, y limit
+        # set x, y limit
         #if minmax == 'self':
         #    plot.set_xlim=(self.xmin, self.xmax)
         #    plot.set_ylim=(self.ymin, self.ymax)
@@ -222,7 +248,7 @@ class MyPlt:
         #else:
         #    print('Unknown mimmax, set minmax to auto')
 
-        # set labels
+        # set labels, shown on axis
         plot.set_xlabel(self.xlabel)
         plot.set_ylabel(self.ylabel)
 
@@ -265,8 +291,10 @@ class MyPlt:
             fig.savefig(file_title + ".png", transparent=False,
                         dpi=100, bbox_inches='tight')
 
-    def xyy_plt(self, ycolor='blue', y2color='red',
-                marker='o', markersize=10, **fig_kw):
+    def xyy_plt(self, ycolor='blue', y2color='red', legend='brief',
+                marker='o', markersize=10,
+                set_ylabel=None, set_y2label=None,
+                **fig_kw):
         """plot data_y1 (on left y axis) and data_y2
         (on right y axis) with same data_x (on x axis).
         data_x, data_y1, data_y2 are 1-d arrays with same length
@@ -276,26 +304,45 @@ class MyPlt:
                 defautl = 'blue', 'red'
             marker: marker of line, default = 'o'
             markersize: marker size, default = 10. 0 means no markers.
+            legend : “brief”, “full”, or False, default = 'brief'
+            set_ylabel, set_y2label: for set_ylabel to set label for y axis
+                None: don't set_ylabel, default
+                'self': use set_ylabel(self.ylabel)
+                other string: use set_ylabel(set_ylabel)
             **fig_kw: keywords that are passed to matplotlib.pyplot.plot
         # Example
-            fig, ax = plt.subplots(2,1,figsize=(14,16))
-            b = PyPlt.MyPlt(df['x'], df['y'], df['y2'], xmin=-1, xmax=5,
-                            ymin=0, ymax=10, y2min=3, y2max=10, y2label='y2',
-                            savefig=True,title='b title')
-            b.xyy_plt(ax=ax[0])
+            (1) x and 2 y axis (left and right):
+            b = PyPlt.MyPlt(df['x'], df['y'], df['y2'],
+                            xmin=-1, xmax=5, ymin=0, ymax=10,
+                            y2min=3, y2max=10, title='b title')
+            b.xyy_plt(legend=False, set_ylabel='its y', set_y2label='its y2',
+                      ax=ax[1])
+
+            (2) multiple data on left y axis
+            c = PyPlt.MyPlt(df['x'], df['y'], ylabel='y',xmin=0.1, xmax=5,
+                            ymin=2, ymax=12)
+            c.xyy_plt(marker='v',ax=ax[2])
+            c = PyPlt.MyPlt(df['x'], df['y2'],ylabel='y2',xmin=0.1, xmax=5,
+                            ymin=2, ymax=12)
+            c.xyy_plt(marker='o',ax=ax[2],set_ylabel='Data')
         # Date
-            20191125
+            20191127
         """
+
+        plot_ylabel = self.ylabel
+        # lebel here will be shown in legend
         ploty = sns.lineplot(self.x, self.y,
-                             color=ycolor, label=self.ylabel,
-                             legend=False, marker=marker,
+                             color=ycolor, label=plot_ylabel,
+                             legend=legend, marker=marker,
                              markersize=markersize,
                              **fig_kw)
+
         if self.double_y:
+            plot_y2label = self.y2label
             ax_y2 = ploty.twinx()
             ploty2 = sns.lineplot(self.x, self.y2,
-                                  color=y2color, label=self.y2label,
-                                  legend=False, marker=marker,
+                                  color=y2color, label=plot_y2label,
+                                  legend=legend, marker=marker,
                                   markersize=markersize,
                                   ax=ax_y2)
 
@@ -305,11 +352,23 @@ class MyPlt:
         if self.double_y:
             ploty2.set_ylim(self.y2min, self.y2max)
 
-        # set labels
+        # set labels, shown on axis
         ploty.set_xlabel(self.xlabel)
-        ploty.set_ylabel(self.ylabel)
+
+        if set_ylabel is None:
+            ploty.set_ylabel('')
+        elif set_ylabel == 'self':
+            ploty.set_ylabel(self.ylabel)
+        else:
+            ploty.set_ylabel(set_ylabel)
+
         if self.double_y:
-            ploty2.set_ylabel(self.y2label)
+            if set_y2label is None:
+                ploty2.set_ylabel('')
+            elif set_y2label == 'self':
+                ploty2.set_ylabel(self.y2label)
+            else:
+                ploty2.set_ylabel(set_y2label)
 
         # set axis color
         if self.double_y:
@@ -318,23 +377,30 @@ class MyPlt:
             ploty2.spines['right'].set_color(ploty2.get_lines()[0].get_color())
         else:
             # use ploty if only 1 y axis is used !!
-            ploty.spines['left'].set_color(ploty.get_lines()[0].get_color())
+            # ploty.spines['left'].set_color(ploty.get_lines()[0].get_color())
+            ploty.spines['left'].set_color('black')
 
         # set color for y labels
-        ploty.yaxis.label.set_color(ploty.get_lines()[0].get_color())
         if self.double_y:
+            ploty.yaxis.label.set_color(ploty.get_lines()[0].get_color())
             ploty2.yaxis.label.set_color(ploty2.get_lines()[0].get_color())
+        else:
+            ploty.yaxis.label.set_color('black')
 
         # set ticks
         tkw = dict(size=4, width=1.5)
         ploty.tick_params(axis='x', **tkw)
-        ploty.tick_params(axis='y',
-                          colors=ploty.get_lines()[0].get_color(),
-                          **tkw)
         if self.double_y:
+            ploty.tick_params(axis='y',
+                              colors=ploty.get_lines()[0].get_color(),
+                              **tkw)
             ploty2.tick_params(axis='y',
                                colors=ploty2.get_lines()[0].get_color(),
                                **tkw)
+        else:
+            ploty.tick_params(axis='y',
+                              colors='black',
+                              **tkw)
 
         # set title
         if self.title is not None:
